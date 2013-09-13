eaa8bd32beccb7fddb0f5fa194b26d7d10719bb1
Brian Ford <btford@umich.edu>
2013-09-13 08:49:52 -0700

    add initial version of dynamic analysis

---
+++ dynamic.js
@@ -0,0 +1,111 @@
+
+// for eval
+global.angular = (function() {
+  'use strict';
+
+  var FN_ARGS = /^function\s*[^\(]*\(\s*([^\)]*)\)/m;
+  var FN_ARG_SPLIT = /,/;
+  var FN_ARG = /^\s*(_?)(\S+?)\1\s*$/;
+  var STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
+
+  function annotate (fn) {
+    var $inject,
+        fnText,
+        argDecl,
+        last;
+
+    if (typeof fn == 'function') {
+      if (!($inject = fn.$inject)) {
+        $inject = [];
+        fnText = fn.toString().replace(STRIP_COMMENTS, '');
+        argDecl = fnText.match(FN_ARGS);
+        argDecl[1].split(FN_ARG_SPLIT).forEach(function(arg) {
+          arg.replace(FN_ARG, function(all, underscore, name) {
+            $inject.push(name);
+          });
+        });
+      }
+    } else if (fn instanceof Array) {
+      last = fn.length - 1;
+      $inject = fn.slice(0, last);
+    }
+    return $inject;
+  }
+
+  function constructArray (fn, dependencies) {
+    return '[' +
+      dependencies.
+        map(function (arg) {
+          return "'" + arg + "'";
+        }).
+        concat([
+          fn.toString()
+        ]).
+        join(', ') +
+      ']';
+  }
+
+  function mock (name, fn) {
+    if (fn instanceof Array) {
+      return module;
+    } else if (typeof fn === 'object') {
+      Object.keys(fn).forEach(function (prop) {
+        mock(null, fn[prop]);
+      });
+      return module;
+    }
+
+    var original = fn.toString();
+    var dependencies = annotate(fn);
+
+    if (dependencies.length === 0) {
+      return module;
+    }
+
+
+    var rewritten = constructArray(original, dependencies);
+
+    global.js = global.js.replace(original, rewritten);
+
+    return module;
+  }
+
+  var module = {
+    controller: mock,
+    directive: mock,
+    filter: mock,
+    service: mock,
+
+    constant: function () { return module; },
+    value: function () { return module; },
+
+    provider: function (name, fn) {
+      mock(name, fn);
+      if (typeof fn === 'function') {
+        var $get = (new fn()).$get;
+        if ($get && typeof $get === 'function') {
+          mock(null, $get);
+        }
+      }
+      return module;
+    },
+    decorator: mock,
+
+    config: function (fn) { return mock(null, fn); },
+    run: function (fn) { return mock(null, fn); }
+  };
+
+  return {
+    module: function () {
+      return module;
+    }
+  };
+}());
+
+
+module.exports = function run (js) {
+  global.js = js;
+  eval(global.js);
+
+  return global.js;
+}
---
+++ test/util.js
@@ -0,0 +1,41 @@
+
+/*
+ * Test utils
+ */
+
+
+var esprima = require('esprima'),
+  escodegen = require('escodegen');
+
+var fnDecl = /^[ ]*function[ ]?\(\)[ ]?\{\n/m,
+  trailingBrace = /[ ]*\}(?![\s\S]*\})/m;
+
+// given a function, return its body as a string.
+// makes tests look a bit cleaner
+exports.stringifyFunctionBody = function (fn) {
+  var out = fn.toString().
+    replace(fnDecl, '').
+    replace(trailingBrace, '');
+
+  return normalize(out);
+};
+
+// given a function, return its body as a string.
+// makes tests look a bit cleaner
+var normalize = exports.normalize = function normalize (fn) {
+  var out = fn.toString();
+
+  // then normalize with esprima/escodegen
+  out = escodegen.generate(
+    esprima.parse(out, {
+      tolerant: true
+    }), {
+      format: {
+        indent: {
+          style: '  '
+        }
+      }
+    });
+
+  return out;
+};
